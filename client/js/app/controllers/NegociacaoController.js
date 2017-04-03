7fc55fd487cd66f66802369653bf3977e101c632
Fabio <Fabio>
2017-04-03 19:49:22 -0300

    importando negociacoes

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -30,6 +30,19 @@ class NegociacaoController {
         this._limparFormulario();
     }
     
+    importaNegociacoes(){
+        
+        let service = new NegociacaoService();
+        service.obterNegociacoesDaSemana((erro, negociacoes) => {
+            if(erro){
+                this._mensagem.texto = erro;
+                return;
+            }
+            negociacoes.forEach(negociacao => this._listaNegociacoes.adiciona(negociacao));
+            this._mensagem.texto = 'Negociacoes importadas com sucesso!';
+        });
+    }
+    
     apaga() {
 
         this._listaNegociacoes.esvazia();
